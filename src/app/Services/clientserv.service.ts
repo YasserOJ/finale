8015cab606b56bc559223df1973eb9a7a63f1dae
YasserOJ <yasser_ess@outlook.com>
2018-06-22 21:08:01 +0100

    ok

--- src/app/Services/clientserv.service.ts
+++ src/app/Services/clientserv.service.ts
@@ -3,10 +3,6 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class ClientservService {
-offer: any;
   constructor(private  http: HttpClient) { }
-  getoffer(tag) {
-    this.http.get('http://localhost:2222/Postule/bytag/' + tag, this.offer).map(res => (<any>res));
-  }
 
 }
